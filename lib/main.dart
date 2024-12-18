f784777bdb79a68662f10f727536abbcbc9e7165
Joyanta Kumar <joyroy122004@gmail.com>
2024-12-18 19:46:35 +0600

    Screen divided for display and keyboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(Calculator());
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: [
+          Expanded(child: Container()),
+          Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.deepPurple,
+              ))
+        ],
+      ),
+    );
+  }
+}
